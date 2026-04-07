f7dbe75d15b7f3c9bc678e791e2acf6b94d098d0
HP Developer <5ynt4x.3rr0r.ss@gmail.com>
2026-04-07 14:30:45 +0530

    graph db

--- database/config.py
+++ database/config.py
@@ -70,9 +70,9 @@ MAX_SAMPLE_VALUES = int(os.getenv("MAX_SAMPLE_VALUES", "100"))
 # NEO4J_USER = os.getenv("NEO4J_USER", "neo4j")
 # NEO4J_PASS = os.getenv("NEO4J_PASS", "Rnpyic2loh-N10dJKWZdnaloP3AJiYPjb7HI1eODTfs")
 
-ARANGO_HOST = os.getenv("ARANGO_HOST", "https://6a262ad8fb39.arangodb.cloud:8529")
+ARANGO_HOST = os.getenv("ARANGO_HOST", "https://e3850a1afb88.arangodb.cloud:8529")
 ARANGO_USER = os.getenv("ARANGO_USER", "root")
-ARANGO_PASS = os.getenv("ARANGO_PASS", "JKigit6UbsKjiuOKpKEF")
+ARANGO_PASS = os.getenv("ARANGO_PASS", "RYQdGYKJavDIheMjmbUK")
 ARANGO_DB = os.getenv("ARANGO_DB", "graph_ai")
 
 # ARANGO created on -- 11/02/2026
